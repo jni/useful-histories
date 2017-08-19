b917f2465032601451b4b8307f136334252dbd61
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-08-19 14:08:24 +0200

    Modify the malaria skeleton script to work interactively

--- malaria-skeleton.py
+++ malaria-skeleton.py
@@ -46,23 +46,29 @@ ridges = ((data['mean shape index'] < 0.625) &
           (data['mean shape index'] > 0.125))
 j2j = data['branch-type'] == 2
 datar = data.loc[ridges & j2j]
+datar['branch distance (nm)'] = datar['branch-distance'] * 1e9
 
 # Plot the full dataset boxplot
-sns.boxplot(data=datar, x='infection', y='branch-distance')
-plt.show(block=True)
+fig, ax = plt.subplots()
+sns.boxplot(data=datar, x='infection', y='branch distance (nm)', ax=ax)
+fig.savefig('simple-boxplot.png')
 
 # Plot the boxplot of the image means
+fig, ax = plt.subplots()
 ## First, group by filename and compute the means
 means = datar.groupby('filename').mean().reset_index()
 ## This deletes non-numeric columns so we need to add them back
 means['infection'] = means['filename'].apply(infection_status)
-sns.boxplot(data=means, x='infection', y='branch-distance')
-plt.show(block=True)
+sns.boxplot(data=means, x='infection', y='branch distance (nm)', ax=ax)
+fig.savefig('grouped-boxplot.png')
 
 # plot the boxplot of the cell means
+fig, ax = plt.subplots()
 cellmeans = datar.groupby(('infection', 'cell number')).mean().reset_index()
-sns.boxplot(x='infection', y='branch-distance', hue='cell number', data=cellmeans)
-plt.show(block=True)
+sns.boxplot(x='infection', y='branch distance (nm)', hue='cell number',
+            data=cellmeans, ax=ax)
+ax.legend_.remove()
+fig.savefig('by-cell-boxplot.png')
 
 _, bins = np.histogram(datar['branch-distance'], bins='auto')
 
@@ -71,4 +77,3 @@ for inf, df in datar.groupby('infection'):
     ax.hist(df['branch-distance'], bins=bins, normed=True,
             alpha=0.5, label=inf)
 ax.legend()
-plt.show(block=True)
