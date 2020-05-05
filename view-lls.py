cfcf752193c8ec6f1c73f1153b1266a7b0ead8bf
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2020-05-05 09:04:27 -0500

    Massively improved view-lls script

--- view-lls.py
+++ view-lls.py
@@ -1,11 +1,29 @@
 # IPython log file
-
-
-import dask.array as d
+import os
+import sys
 import dask.array as da
-lls = da.from_zarr('data/gokul-lls/aollsm-m4-560nm.zarr')
-lls = da.from_zarr('gokul-lls/aollsm-m4-560nm.zarr')
-lls2 = da.from_zarr('gokul-lls/aollsm-m4-560nm-downscale.zarr')
 import napari
-v = napari.view_image([lls, lls2], is_pyramid=True,
-    contrast_limits=[0, 150000])
+from napari.utils import resize_dask_cache
+
+if sys.platform == 'linux':
+    media = '/media/jni'
+elif sys.platform == 'darwin':
+    media = '/Volumes'
+
+lls = da.from_zarr(
+    os.path.join(media, 'datac/napari-data/gokul-lls/aollsm-m4-560nm.zarr')
+)
+lls2 = da.from_zarr(
+    os.path.join(
+        media, 'datac/napari-data/gokul-lls/aollsm-m4-560nm-downscale.zarr'
+    )
+)
+
+with napari.gui_qt():
+    v = napari.view_image(
+        lls_560nm := [lls, lls2],
+        multiscale=True,
+        contrast_limits=[0, 150000],
+        scale=(1, 3, 1, 1),
+    )
+    resize_dask_cache(200e6)
